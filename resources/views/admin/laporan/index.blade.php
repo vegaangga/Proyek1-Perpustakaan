64e7f5fa61ae79cb193c7c5dc6ac37cbac0f7aba
NadiaLayraAziza <nadialayra15@gmail.com>
2021-04-30 22:01:49 +0700

    Ganti Logo

--- resources/views/admin/laporan/index.blade.php
+++ resources/views/admin/laporan/index.blade.php
@@ -8,25 +8,25 @@
                     <h4>Cetak Laporan PDF</h4>
                 </div>
                 <div class="panel-body" style="text-align: center">
-                    <div class="alert alert-success">
+                    {{--  <div class="alert alert-success">
                         <p class="alert-primary">Note: Ubah Controller e dulu lek mau download pdf awkwk </p>
-                    </div>
+                    </div>  --}}
 
                     <a href="{{ route('laporan.buku') }}" class="btn btn-primary" style="margin-bottom: 5px">Cetak Data Buku</a>
                     <a href="{{ route('laporan.anggota') }}" class="btn btn-danger" style="margin-bottom: 5px">Cetak Data Anggota</a>
                     <a href="{{ route('laporan.transaksi') }}" class="btn btn-success" style="margin-bottom: 5px">Cetak Data Transaksi</a>
 
-                    <div class="alert alert-info">
+                    {{--  <div class="alert alert-info">
                         <p>Note2: Lek udah ngerubah ke download, logo smk ga mau muncul ehehehe &#128514;</p>
-                    </div>
+                    </div>  --}}
 
-                    <div class="alert alert-danger">
+                    {{--  <div class="alert alert-danger">
                         <p>Karena saya sudah ngantuk, Tolong dibenerin awkwkwkwk &#128525;</p>
 
-                    </div>
-                    <div class="alert alert-success">
+                    </div>  --}}
+                    {{--  <div class="alert alert-success">
                         <p>Maklum stuck jadi refreshing sek  &#128553;</p>
-                    </div>
+                    </div>  --}}
                 </div>
             </div>
         </div>
